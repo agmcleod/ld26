1c8f51b56e68a2406defad4c50ec2ef9f10486f1
Aaron McLeod <sircoolguy99@gmail.com>
2013-04-27 00:31:16 -0400

    barrel now follows mouse

--- src/ld26/Barrel.java
+++ src/ld26/Barrel.java
@@ -1,15 +1,30 @@
 package ld26;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Barrel extends Entity {
+	
+	private float angle;
+	
 	public Barrel(TextureRegion texture) {
 		super(texture, 120, 110);
+		angle = 0;
+	}
+	
+	public float getAngle() {
+		return this.angle;
 	}
 	
 	@Override
 	public void render(SpriteBatch batch) {
-		batch.draw(getTexture(), getPos().x, getPos().y, 0, 0, 8, 41, 1, 1, 270);
+		batch.draw(getTexture(), getPos().x, getPos().y, 0, 0, 32, 8, 1, 1, this.angle, true);
+	}
+	
+	public void setAngle(float angle) {
+		if((angle > 0 && angle < 90) || (angle < 360 && angle > 270)) {
+			this.angle = angle;
+		}
 	}
 }
--- src/ld26/PlayScreen.java
+++ src/ld26/PlayScreen.java
@@ -1,20 +1,27 @@
 package ld26;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
-public class PlayScreen implements Screen {
+public class PlayScreen implements Screen, InputProcessor {
 	
 	private Texture background;
 	private SpriteBatch batch;
 	private Barrel barrel;
+	private OrthographicCamera camera;
 	private Game game;
 	private TextureRegion potato;
 	private Texture sprites;
+	private Vector2 targetPos;
 	private TextureRegion tower;
 	
 	
@@ -35,6 +42,32 @@ public class PlayScreen implements Screen {
 	}
 
 	@Override
+	public boolean keyDown(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int x, int y) {
+		Vector3 pos = new Vector3(x, y, 0);
+		camera.unproject(pos);
+		targetPos.set(pos.x, pos.y);
+		return true;
+	}
+
+	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
 
@@ -42,6 +75,7 @@ public class PlayScreen implements Screen {
 
 	@Override
 	public void render(float arg0) {
+		update();
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background, 0, 0);
@@ -64,13 +98,62 @@ public class PlayScreen implements Screen {
 	}
 
 	@Override
+	public boolean scrolled(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void setAngleViaMouseCoords() {
+		Vector2 bPos = barrel.getPos();
+		float angle = MathUtils.atan2(targetPos.y - bPos.y, targetPos.x - bPos.x) * (180 / MathUtils.PI);
+		if(angle < 0) {
+			angle = 360 - (-angle);
+		}
+		barrel.setAngle(angle);
+	}
+
+	@Override
 	public void show() {
 		background = new Texture(Gdx.files.internal("assets/background.png"));
 		batch = new SpriteBatch();
 		sprites = new Texture(Gdx.files.internal("assets/potato.png"));
 		potato = new TextureRegion(sprites, 0, 0, 64, 128);
 		tower = new TextureRegion(sprites, 64, 0, 64, 128);
-		barrel = new Barrel(new TextureRegion(sprites, 128, 0, 8, 41));
+		barrel = new Barrel(new TextureRegion(sprites, 128, 0, 32, 8));
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false);
+		
+		Vector3 startPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+		camera.unproject(startPos);
+		
+		targetPos = new Vector2(startPos.x, startPos.y);
+		
+		Gdx.input.setInputProcessor(this);
+	}
+
+	@Override
+	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void update() {
+		camera.update();
+		// update barrel angle
+		setAngleViaMouseCoords();
 	}
 
 }
